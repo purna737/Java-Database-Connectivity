3ba3110f7711db3bd7486d67a65c0d05129e8777
Purna <purnanaidu830@gmail.com>
2026-08-20 23:02:43 +0530

    Add files via upload

---
+++ Jdbcjavaapp02/src/main/java/jdbcjavaapp/App2.java
@@ -0,0 +1,86 @@
+package jdbcjavaapp;
+
+
+
+
+
+import java.sql.*;
+
+public class App2
+{
+    public static void main( String[] args )
+
+    {
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root" , "Root@735");
+
+            stmt= conn.createStatement();
+
+            String SqlQuery = """
+                    SELECT id,name,sal from empj
+                    """;
+
+            rs = stmt.executeQuery(SqlQuery);
+
+          while(rs.next())
+            {
+               /* int id = rs.getInt(1);
+                String name = rs.getString(2);
+                int sal = rs.getInt(3);*/
+
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int sal = rs.getInt("sal");
+
+
+                System.out.println("Employee id :" + id);
+                System.out.println("Employee name :" + name);
+                System.out.println("Employee sal :" + sal);
+                System.out.println();
+
+            }
+        }
+        catch(ClassNotFoundException cnfex)
+        {
+            cnfex.printStackTrace();
+        }
+        catch(SQLException sqex)
+        {
+            sqex.printStackTrace();
+
+            System.out.println("Error saving Record");
+        }
+
+        finally {
+            try{
+
+                if(rs !=null)
+                {
+                    rs.close();
+                }
+                if(stmt!=null)
+                {
+                    stmt.close();
+                }
+                if(conn!=null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException sqel)
+            {
+                sqel.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ Jdbcjavaapp02/src/main/java/jdbcjavaapp/App3.java
@@ -0,0 +1,78 @@
+package jdbcjavaapp;
+
+import java.sql.*;
+
+public class App3 {
+
+    public static void main (String[] args)
+    {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+
+
+        try {
+           Class.forName("com.mysql.cj.jdbc.Driver");
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root" , "Root@735");
+            stmt = conn.createStatement();
+
+
+           /* String SqlQuery = """
+                    INSERT INTO employee VALUES(101,'jai','kumar','2021-12-11'),
+                    (102,'kumar','raj','2023-03-18'),(103,'pavan','kumar','2024-10-02'),
+                    (104,'vishnu','teja','2021-02-11')
+                    """;*/
+
+
+           /* String SqlQuery = " SELECT  first_name,last_name from employee";
+
+
+            rs = stmt.executeQuery(SqlQuery);
+            while (rs.next())
+            {
+                String first_name  = rs.getString(1);
+                String last_name = rs.getString(2);
+
+                System.out.println("First name is :"+first_name);
+                System.out.println("last name is: "+last_name);
+                System.out.println();
+            }*/
+
+
+
+            String 
+
+        }
+
+
+
+
+        catch (ClassNotFoundException cnfex)
+        {
+            cnfex.printStackTrace();
+        }
+        catch (SQLException sqe)
+        {
+            sqe.printStackTrace();
+        }
+        finally {
+            try{
+
+                if(stmt!=null)
+                {
+                    stmt.close();
+                }
+                if(conn!=null)
+                {
+                    conn.close();
+                }
+            }
+            catch (SQLException sqel)
+            {
+                sqel.printStackTrace();
+            }
+        }
+    }
+}
