62015247cecc4bf453f7c4b30b668fb26cfe1ab8
Purna <purnanaidu830@gmail.com>
2026-08-20 23:03:47 +0530

    Remove sensitive information and clean up code

--- Jdbcjavaapp02/src/main/java/jdbcjavaapp/App2.java
+++ Jdbcjavaapp02/src/main/java/jdbcjavaapp/App2.java
@@ -21,7 +21,7 @@ public class App2
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded");
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root" , "Root@735");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc", "root" , "*****");
 
             stmt= conn.createStatement();
 
